9184d91859e728877db6e9bf1ab5cc4703ae04a2
d <dhenton@localhost.com>
2015-06-30 08:20:59 -0500

    added clipboard copy, added selection box, added format for error reporting

---
+++ public_html/js/comparison_code.js
@@ -0,0 +1,58 @@
+//will be loaded by jquery on pageLoad
+var failedData = null;
+
+
+function doCompare()
+
+{
+    var selectIdx = $('#testList').val();
+    var expectedStr = 
+    JSON3.stringify(failedData["comparisons"][selectIdx].expected,null,2);
+    var actualStr = 
+    JSON3.stringify(failedData["comparisons"][selectIdx].actual,null,2);
+    var wikEdDiff = new WikEdDiff();
+//    wikEdDiffConfig.fullDiff = true;
+//    wikEdDiffConfig.showBlockMoves = false;
+    $('#expected').val(expectedStr);
+    $('#actual').val(actualStr);
+    
+    var diffHtml = wikEdDiff.diff(expectedStr, actualStr);
+    $('#compare').empty();
+    $('#compare').html(diffHtml);
+    if (!$('#legend').is(":visible"))
+    {
+        $('#legend').show();
+        $('#copy-button').css("visibility", "visible");
+    }
+    
+}
+
+
+function loadSelections()
+{
+    
+    var listObj = $('#testList');
+    var firstChoice = false;
+    failedData["comparisons"].forEach(function (d, i)
+    {
+        var t = "";
+        if (firstChoice === false)
+        {
+            t = "<option selected value=\"" + i + "\">" + getDescription(d) + "</option>";
+            firstChoice = true;
+        }
+        else
+        {
+            t = "<option value=\"" + i + "\">" + getDescription(d) + "</option>";
+        }
+
+        listObj.append(t);
+
+    });
+    
+}
+
+function getDescription(testItem)
+{
+    return testItem.testName+"-->"+testItem.methodName;
+}
