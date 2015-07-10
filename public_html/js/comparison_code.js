eb7b72bc65f79387766b1756fdba7560af9e658b
d <dhenton@localhost.com>
2015-07-10 10:51:59 -0500

    dropping zeroclipboard for standard setup of tools for json formatting

--- public_html/js/comparison_code.js
+++ public_html/js/comparison_code.js
@@ -1,20 +1,95 @@
+///////////////// formatter functions ////////////////////////
+function copyToFormatter()
+{
+    var selectIdx = $('#testList').val();
+    var actualStr =
+            JSON3.stringify(failedData["comparisons"][selectIdx].actual);
+    $('#formatArea').val(actualStr);
+    $('#navTabs a:last').tab('show') // Select last tab
 
+}
+
+function prettyPrintFormatter()
+{
+    var json = getToolTextASJSON();
+    if (json != null)
+    {
+        $('#formatArea').val(JSON3.stringify(json, null, 3));
+        reportParseProblem(false);
+    }
+    else
+    {
+        reportParseProblem(true);
+    }
+
+}
+function unFormatFormatter()
+{
+    var json = getToolTextASJSON();
+    if (json != null)
+    {
+        $('#formatArea').val(JSON3.stringify(json));
+        reportParseProblem(false);
+    }
+    else
+    {
+        reportParseProblem(true);
+    }
+
+}
+
+
+function reportParseProblem(show)
+{
+    if (show == true)
+    {
+        $("#parseWarning").text("Problem parsing JSON");
+    }
+    else
+    {
+         $("#parseWarning").text("");
+    }
+}
+
+function getToolTextASJSON()
+{
+    var t = $('#formatArea').val();
+    if (t != null)
+    {
+        t = t.trim();
+    }
+    else
+    {
+        t = "";
+    }
+    var jsonInfo = null;
+    try
+    {
+        jsonInfo = $.parseJSON(t);
+    }
+    catch (e)
+    {
+
+    }
+    return jsonInfo;
+}
+
+/////////////////////////////////////////////////////////////
 
 
 function doCompare()
 
 {
     var selectIdx = $('#testList').val();
-    var expectedStr = 
-    JSON3.stringify(failedData["comparisons"][selectIdx].expected,null,2);
-    var actualStr = 
-    JSON3.stringify(failedData["comparisons"][selectIdx].actual,null,2);
+    var expectedStr =
+            JSON3.stringify(failedData["comparisons"][selectIdx].expected, null, 2);
+    var actualStr =
+            JSON3.stringify(failedData["comparisons"][selectIdx].actual, null, 2);
     var wikEdDiff = new WikEdDiff();
 //    wikEdDiffConfig.fullDiff = true;
 //    wikEdDiffConfig.showBlockMoves = false;
     $('#expected').val(expectedStr);
     $('#actual').val(actualStr);
-    
     var diffHtml = wikEdDiff.diff(expectedStr, actualStr);
     $('#compare').empty();
     $('#compare').html(diffHtml);
@@ -23,16 +98,15 @@ function doCompare()
         $('#legend').show();
         $('#copy-button').css("visibility", "visible");
     }
-    
+
 }
 
 
 function loadSelections()
 {
-    
+
     var listObj = $('#testList');
     var firstChoice = false;
-     
     failedData["comparisons"].forEach(function (d, i)
     {
         var t = "";
@@ -47,16 +121,11 @@ function loadSelections()
         }
 
         listObj.append(t);
-
     });
-    
-
-    $('#envInfo').text(failedData.date+" ("+failedData.env+")");
-   
-    
+    $('#envInfo').text(failedData.date + " (" + failedData.env + ")");
 }
 
 function getDescription(testItem)
 {
-    return testItem.testName+"-->"+testItem.methodName;
+    return testItem.testName + "-->" + testItem.methodName;
 }
